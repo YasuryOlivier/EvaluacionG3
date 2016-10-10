037d7aa8a3312f1629bac9c51fb0273cd01241eb
YasuryOlivier <olivierbarriosyasury@gmail.com>
2016-10-10 16:26:43 -0500

    ejercicios

---
+++ Ejercicios2daParte/ejercicio3.js
@@ -0,0 +1,5 @@
+function palidromo(palabra){
+  var frase=palabra.replace;
+  for (var i=0; i<=frase.length; i++) {
+ /*hasta aquí recuerdso de como se hace eñl ejercicio de función*/  }
+}
---
+++ Ejercicios2daParte/ejercicio4.js
@@ -0,0 +1,17 @@
+var persona(){
+	nombre:;
+	edad:;
+	genero:;
+}
+var obDetalles;
+/*************************************************************/
+var estudiante(){
+	curso:;
+	grupo:;
+	metodo:;
+}
+/************************************************************/
+var profesor(){
+	asignatura:;
+	nivel:;
+}
