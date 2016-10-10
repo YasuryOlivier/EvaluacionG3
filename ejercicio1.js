d8f1f43310652c8ff37826e5b39a04f8f78059e2
YasuryOlivier <olivierbarriosyasury@gmail.com>
2016-10-10 13:13:23 -0500

    Evaluación

---
+++ ejercicio1.js
@@ -0,0 +1,27 @@
+/*
+1.-Inicio 
+2.-	Pide al usuario que ingrese Piedra, Papel ó Tijera.
+3.- Pide al usuario que ingrese Piedra, Papel ó Tijera, sin repetir el anterios
+4.- Si el usuario ingresa papel y tijera entonces imprime gana tijera.
+5.- si el usuario ingresa piedra y papel entonces imprime gana papel.
+6.- si el usuario ingresa tijera y piedra entonces imprime gana piedra.
+8.- si el usuario ingresa dos valores identicos entonces imprime es un empate 
+7.Fin  
+*/
+
+var ganarUno=prompt("ingrese Piedra, Papel ó Tijera.");
+var ganarDos=prompt("ingrese Piedra, Papel ó Tijera.");
+var piedra;
+var papel;
+var tijera;
+
+if (papel =tijeras){
+	console.log ("gana tijeras");
+}
+else-if(tijera=piedra)
+{
+	console.log("gana piedra");
+}
+else{
+	console.log("gana papel");
+}*/
---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+/*Ingresa nombre 1, Ingresa nombre 2, ingresa nombre 3*/ 
+/*se que es un arreglo, pero no puedo hacerlo, o  no recuerdo :( */
+prompt("Ingresa Nombre 1");
+prompt("Ingresa Nombre 2");
+prompt("Ingresa Nombre 3");
+
+
+var nombres=["", "","" ];
+nombres[];
